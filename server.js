7a24a9af4fc1252d7eb34b0206e97a7fcb14a411
rajeshchinnaga <39455723+rajeshchinnaga@users.noreply.github.com>
2021-04-19 21:52:19 -0400

    NoSQL Assignment Final Changes

--- server.js
+++ server.js
@@ -31,4 +31,4 @@ require('./controllers/quizzes-controller')(app)
 require('./controllers/questions-controller')(app)
 require('./controllers/quiz-attempts-controller')(app)
 
-app.listen(5000)
+app.listen(process.env.PORT ||5000)
